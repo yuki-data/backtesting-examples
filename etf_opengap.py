b6291c115315901e950d62db65f87ab830563013
yuki-data <45279969+yuki-data@users.noreply.github.com>
2020-03-04 19:33:28 +0900

    open gapインジケータを作成: strategyから参照するため

--- etf_opengap.py
+++ etf_opengap.py
@@ -1,7 +1,22 @@
-import backtradr as bt
+"""
+created 200304
+
+open gapで1%上がったら買うstrategy。利益が1%出たら利確する。
+"""
+
+import backtrader as bt
 
 
-class BuyAndHold(bt.Strategy):
+class Opengap(bt.Indicator):
+    params = (("gapup_threshold", 1),)
+    lines = ("open_gap_up",)
+
+    def __init__(self):
+        pct_relative_open = bt.DivByZero(self.data0.lines.open - self.data0.lines.close(-1), self.data0.lines.close(-1), bt.NAN)
+        self.lines.open_gap_up = (pct_relative_open * 100) > self.p.gapup_threshold
+
+
+class ETFOpengapStrategy(bt.Strategy):
     def log(self, txt, dt=None):
         dt = dt or self.datas[0].datetime.date(0)
         print(dt, txt)
@@ -9,6 +24,8 @@ class BuyAndHold(bt.Strategy):
     def __init__(self):
         self.order = None
         self.dataclose = self.data0.lines.close
+        self.dateopen = self.data0.lines.open
+        self.buy_signal = Opengap(gapup_threshold=1).lines.open_gap_up
 
     def notify_order(self, order):
         if order.status in [order.Submitted, order.Accepted]:
@@ -26,21 +43,24 @@ class BuyAndHold(bt.Strategy):
 
     def next(self):
         # self.log('Close, %.2f' % self.dataclose[0])
+        # self.log('self.buy_signal, %.2f' % self.buy_signal[0])
         self.go_long()
 
     def recommend_buy(self):
-        return True
+        if self.buy_signal == 1:
+            return True
 
-    def action_buy(self):
+    def execute_buy(self):
         self.log('BUY CREATE, %.2f' % self.dataclose[0])
         # self.order = self.buy(exectype=bt.Order.Market, coc=False)
         # self.order = self.buy(exectype=bt.Order.Close)
-        self.order = self.buy(coo=False, coc=True, exectype=bt.Order.Market)
+        self.order = self.buy(exectype=bt.Order.Market, coc=False)
+        # self.order = self.buy(coo=False, coc=True, exectype=bt.Order.Market)
 
     def recommend_sell(self):
-        return
+        return len(self) >= (self.bar_executed + 2)
 
-    def action_sell(self):
+    def execute_sell(self):
         self.log('SELL CREATE, %.2f' % self.dataclose[0])
         self.order = self.sell(exectype=bt.Order.Market)
 
@@ -49,7 +69,7 @@ class BuyAndHold(bt.Strategy):
             return
         if not self.position:
             if self.recommend_buy() is True:
-                self.action_buy()
+                self.execute_buy()
         else:
             if self.recommend_sell() is True:
                 self.execute_sell()
