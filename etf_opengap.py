0926ee6bf464eee4a832a82dfe7c1a39a5f126c4
yuki-data <45279969+yuki-data@users.noreply.github.com>
2020-03-05 14:36:59 +0900

    バックテストに必要な機能を一つのファイルにまとめるためanalyzerを追加

--- etf_opengap.py
+++ etf_opengap.py
@@ -7,6 +7,31 @@ open gapで1%上がったら買うstrategy。利益が1%出たら利確する。
 import backtrader as bt
 
 
+class MyAnalyzer(bt.Analyzer):
+    """
+    # Usage
+    cerebro.addanalyzer(MyAnalyzer, _name='myanalyzer')
+    strats = cerebro.run()
+    strat = thestrats[0]
+    print('Sharpe Ratio:', strat.analyzers.myanalyzer.get_analysis())
+    """
+
+    def start(self):
+        self.results = {
+            "cash": [],
+            "portfolio": [],
+            "date": []
+        }
+
+    def next(self):
+        self.results["cash"].append(self.strategy.broker.get_cash())
+        self.results["portfolio"].append(self.strategy.broker.getvalue())
+        self.results["date"].append(self.datas[0].datetime.datetime(0))
+
+    def get_analysis(self):
+        return self.results
+
+
 class Opengap(bt.Indicator):
     params = (("gapup_threshold", 1),)
     lines = ("open_gap_up",)
