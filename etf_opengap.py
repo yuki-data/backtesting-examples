57ea3b30cbd699405d0b415fea8632e9a4e95712
yuki-data <45279969+yuki-data@users.noreply.github.com>
2020-03-05 14:39:01 +0900

    cerebro.runまわりをまとめたヘルパー関数を追加

--- etf_opengap.py
+++ etf_opengap.py
@@ -104,3 +104,32 @@ class ETFOpengapStrategy(bt.Strategy):
 
     def after_order_completed(self, order):
         self.bar_executed = len(self)
+
+
+def run_bt(data, Strategy, Analyzer=None, analyzer_name="myanalyzer", cheat_on_open=True, strategy_kwargs={}):
+    cerebro = bt.Cerebro(stdstats=False, cheat_on_open=cheat_on_open)
+    cerebro.broker.set_coc(True)
+    cerebro.addstrategy(Strategy, **strategy_kwargs)
+    # Add the Data Feed to Cerebro
+    cerebro.adddata(data)
+
+    if Analyzer:
+        cerebro.addanalyzer(Analyzer, _name=analyzer_name)
+
+    # Set our desired cash start
+    cerebro.broker.setcash(100000.0)
+    # Print out the starting conditions
+    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    # オブザーバーのセット
+    cerebro.addobserver(bt.observers.DrawDown)
+    # cerebro.addobserver(bt.observers.Trades) # ここの取引での損益
+    cerebro.addobserver(bt.observers.BuySell)  # buy-sellボイントでのマーカー
+    cerebro.addobserver(bt.observers.Value)  # 資産曲線
+    # Run over everything
+    thestrats = cerebro.run()
+    setattr(cerebro, "thestrats", thestrats)
+
+    # Print out the final result
+    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    # cerebro.plot()
+    return cerebro
