03afea8d6d00f2e87f04fab3ac67ea970dd7f4c2
yuki-data <45279969+yuki-data@users.noreply.github.com>
2020-03-08 18:27:58 +0900

    run_btでビルトインのbt.analyzersを追加可能にした

--- backtester/bt_runners/etf_opengap_entry.py
+++ backtester/bt_runners/etf_opengap_entry.py
@@ -3,7 +3,8 @@ import backtrader as bt
 
 def run_bt(data, Strategy, Analyzer=None, analyzer_name="myanalyzer",
            trade_size=1, initial_cash=100000, commission_pct_rate=0.1,
-           cheat_on_open=True, cheat_on_close=True, strategy_kwargs={}):
+           cheat_on_open=True, cheat_on_close=True, strategy_kwargs={},
+           builtin_analyzers=None):
     cerebro = bt.Cerebro(stdstats=False, cheat_on_open=cheat_on_open)
     cerebro.addsizer(bt.sizers.SizerFix, stake=trade_size)
 
@@ -15,6 +16,9 @@ def run_bt(data, Strategy, Analyzer=None, analyzer_name="myanalyzer",
 
     if Analyzer:
         cerebro.addanalyzer(Analyzer, _name=analyzer_name)
+    if builtin_analyzers:
+        for analyzer in builtin_analyzers:
+            cerebro.addanalyzer(analyzer)
 
     # Set our desired cash start
     cerebro.broker.setcash(initial_cash)
